222a2abca9c00ef0af01864f8b4ffb5361f83bdc
AvalonCV <avaloncv@gmail.com>
2021-03-05 07:03:16 +0100

    Package update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,31 @@ interface AppProps {
 
 interface AppState { }
 
+
+const DemoModal: React.FunctionComponent<{}> = () => {
+    return (
+        <FelaComponent as="div" style={{
+            backgroundColor: 'white',
+            maxWidth: '600px',
+            boxShadow: '0 0 2em gray',
+            marginTop: '3em',
+            marginLeft: 'auto',
+            marginRight: 'auto'
+        }}>
+            <FelaComponent as="div" style={{ backgroundColor: '#324c59', color: 'lightgray', minHeight: '4em' }}>
+                Überschrift
+            </FelaComponent>
+            <FelaComponent as="div" style={{ backgroundColor: 'meganta' }}>
+                Quickinfo
+            </FelaComponent>
+            <FelaComponent as="div" style={{ backgroundColor: 'lime' }}>
+                content
+            </FelaComponent>
+        </FelaComponent>
+    );
+}
+
+
 export class App extends React.PureComponent<AppProps, AppState> {
     public render(): JSX.Element {
         return (
@@ -17,6 +42,7 @@ export class App extends React.PureComponent<AppProps, AppState> {
                     <React.Fragment>
                         <FelaComponent as="h1" style={{ color: 'firebrick' }}>Welcome back!</FelaComponent>
                         <div>Fancy that this still works?</div>
+                        <DemoModal />
                     </React.Fragment>
                 </ThemeProvider>
             </RendererProvider >
